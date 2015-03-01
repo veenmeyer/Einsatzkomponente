40114ddd611b12cf4bf7a2c2a157eecf5ac7e1e4
Ralf Meyer <ralf.meyer@mail.de>
2015-03-01 21:16:17 +0100

    no Changes

--- site/controllers/einsatzfahrzeuge.php
+++ site/controllers/einsatzfahrzeuge.php
@@ -1,29 +1,29 @@
-<?php
-/**
- * @version     3.0.0
- * @package     com_einsatzkomponente
- * @copyright   Copyright (C) by Ralf Meyer 2013. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- * @author      Ralf Meyer <webmaster@feuerwehr-veenhusen.de> - http://einsatzkomponente.de
- */
-
-// No direct access.
-defined('_JEXEC') or die;
-
-require_once JPATH_COMPONENT.'/controller.php';
-
-/**
- * Einsatzfahrzeuge list controller class.
- */
-class EinsatzkomponenteControllerEinsatzfahrzeuge extends EinsatzkomponenteController
-{
-	/**
-	 * Proxy for getModel.
-	 * @since	1.6
-	 */
-	public function &getModel($name = 'Einsatzfahrzeuge', $prefix = 'EinsatzkomponenteModel')
-	{
-		$model = parent::getModel($name, $prefix, array('ignore_request' => true));
-		return $model;
-	}
+<?php
+/**
+ * @version     3.0.0
+ * @package     com_einsatzkomponente
+ * @copyright   Copyright (C) by Ralf Meyer 2013. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @author      Ralf Meyer <webmaster@feuerwehr-veenhusen.de> - http://einsatzkomponente.de
+ */
+
+// No direct access.
+defined('_JEXEC') or die;
+
+require_once JPATH_COMPONENT.'/controller.php';
+
+/**
+ * Einsatzfahrzeuge list controller class.
+ */
+class EinsatzkomponenteControllerEinsatzfahrzeuge extends EinsatzkomponenteController
+{
+	/**
+	 * Proxy for getModel.
+	 * @since	1.6
+	 */
+	public function &getModel($name = 'Einsatzfahrzeuge', $prefix = 'EinsatzkomponenteModel')
+	{
+		$model = parent::getModel($name, $prefix, array('ignore_request' => true));
+		return $model;
+	}
 }
