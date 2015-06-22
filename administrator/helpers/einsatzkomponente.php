02659f47c4db1ed1a3502366c88c931de222471c
Ralf Meyer <ralf.meyer@mail.de>
2015-06-22 20:02:23 +0200

    Bugfix: Navigation-Buttons
    http://www.einsatzkomponente.de/wpbt/index.php/Issue/131-CSS-Einstellungen-der-Navigationsbutton-werden-nicht-ber%C3%BCcksichtigt/?commentID=471#comment471

--- administrator/helpers/einsatzkomponente.php
+++ administrator/helpers/einsatzkomponente.php
@@ -623,20 +623,15 @@ return $gmap; }
 	    public static function getNavbar($params,$prev_id,$next_id,$id,$menu_link) {
 	
 	$navbar  ='';
-	//$navbar .='<ul class="pager">';
-	//$navbar .='<ul class="pagination pagination-lg">';
-	//$navbar .='<ul class="pagination">';
 	$navbar .='<div class="btn-group-justified">';
 	
 	if( $prev_id) : 
-    $navbar .='<a href="'.JRoute::_('index.php?option=com_einsatzkomponente&view=einsatzbericht&id=' . (int)$prev_id).'" class="btn btn-primary eiko_btn_2" title="">';
-    //$navbar .='<a href="'.JURI::base().'index.php?option=com_einsatzkomponente&view=einsatzbericht&id=' . (int)$prev_id.'" class="eiko_btn_2" title="'.$prev_id.'">';
+    $navbar .='<a href="'.JRoute::_('index.php?option=com_einsatzkomponente&view=einsatzbericht&id=' . (int)$prev_id).'" class="btn eiko_btn_2" title="">';
     $navbar .='<strong>Zurück</strong></a>';
 	endif; 
 	
 	if( $next_id) :
-    $navbar .='<a href="'.JRoute::_('index.php?option=com_einsatzkomponente&view=einsatzbericht&id=' . (int)$next_id).'" class=" btn btn-primary eiko_btn_2" title="">';
-    //$navbar .='<a href="'.JURI::base().'index.php?option=com_einsatzkomponente&view=einsatzbericht&id=' . (int)$next_id.'" class="eiko_btn_2" title="'.$next_id.'">';
+    $navbar .='<a href="'.JRoute::_('index.php?option=com_einsatzkomponente&view=einsatzbericht&id=' . (int)$next_id).'" class=" btn eiko_btn_2" title="">';
     $navbar .='<strong>Vor</strong></a>';
 	endif; ?>
     
@@ -646,15 +641,13 @@ return $gmap; }
 			?>
     
 	<?php if( $menu_link) :  
-    $navbar .='<a href="'.$menu_link.'" class="btn btn-primary btn-details"><strong>Übersicht</strong></a>';
-    //$navbar .='<a href="'.$menu_link.'" class="btn-details"><span><strong>Übersicht</strong></span></a>';
+    $navbar .='<a href="'.$menu_link.'" class="btn btn-details"><strong>Übersicht</strong></a>';
 	endif;
 	if( !$menu_link) :
-    $navbar .='<a href="'.JRoute::_('index.php?option=com_einsatzkomponente&view=einsatzberichte&Itemid='.$params->get('homelink','').'').'" class="btn btn-primary eiko_btn_2"><strong>Übersicht</strong></a>';
-    //$navbar .='<a href="'.JURI::base().'index.php?option=com_einsatzkomponente&view=einsatzberichte" class="eiko_btn_2"><span><strong>Übersicht</strong></span></a>';
+    $navbar .='<a href="'.JRoute::_('index.php?option=com_einsatzkomponente&view=einsatzberichte&Itemid='.$params->get('homelink','').'').'" class="btn eiko_btn_2"><strong>Übersicht</strong></a>';
 	endif; 
 	if(JFactory::getUser()->authorise('core.edit.own', 'com_einsatzkomponente.einsatzbericht.'.$id)):
-    $navbar .='<a href="'.JRoute::_('index.php?option=com_einsatzkomponente&view=einsatzberichtform&layout=edit&id='.$id).'" class=" btn btn-primary eiko_btn_2">';
+    $navbar .='<a href="'.JRoute::_('index.php?option=com_einsatzkomponente&view=einsatzberichtform&layout=edit&id='.$id).'" class=" btn eiko_btn_2">';
     $navbar .='<strong>Editieren</strong></a>';
     endif;
 	
