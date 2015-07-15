7c508c5e9ab123f9ddea6adbe716759d90dcdc19
Simon W <MrWeezle@users.noreply.github.com>
2015-07-15 15:07:42 +0200

    Update einsatzbericht.php

--- administrator/controllers/einsatzbericht.php
+++ administrator/controllers/einsatzbericht.php
@@ -37,7 +37,7 @@ class EinsatzkomponenteControllerEinsatzbericht extends JControllerForm
 	//$pdf->Cell(120,10,$result['address']);
 	//$pdf->Cell(20,10,$result['summary']);
 	$pdf->Cell(40,10,'Hallo Welt');
-	$pdf->Output('testdatei.pdf','S');
+	$pdf->Output('testdatei.pdf','F');
      	
      }
      function swf()  
