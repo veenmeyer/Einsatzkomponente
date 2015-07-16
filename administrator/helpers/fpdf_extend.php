d597a02153798b2a33fad08b98d792b8cd23dc88
Simon W <MrWeezle@users.noreply.github.com>
2015-07-16 14:56:23 +0200

    Create fpdf_extend.php

---
+++ administrator/helpers/fpdf_extend.php
@@ -0,0 +1,48 @@
+<?php
+/* Caveat: I'm not a PHP programmer, so this may or may
+ * not be the most idiomatic code...
+ *
+ * FPDF is a free PHP library for creating PDFs:
+ * http://www.fpdf.org/
+ */
+require("fpdf.php");
+
+class PDF extends FPDF {
+
+    const DPI = 96;
+    const MM_IN_INCH = 25.4;
+    const A4_HEIGHT = 297;
+    const A4_WIDTH = 210;
+    // tweak these values (in pixels)
+    const MAX_WIDTH = 800;
+    const MAX_HEIGHT = 500;
+
+    function pixelsToMM($val) {
+        return $val * self::MM_IN_INCH / self::DPI;
+    }
+
+    function resizeToFit($imgFilename) {
+        list($width, $height) = getimagesize($imgFilename);
+
+        $widthScale = self::MAX_WIDTH / $width;
+        $heightScale = self::MAX_HEIGHT / $height;
+
+        $scale = min($widthScale, $heightScale);
+
+        return array(
+            round($this->pixelsToMM($scale * $width)),
+            round($this->pixelsToMM($scale * $height))
+        );
+    }
+
+    function centreImage($img, $x, $y) {
+        list($width, $height) = $this->resizeToFit($img);
+
+        // you will probably want to swap the width/height
+        // around depending on the page's orientation
+        $this->Image(
+            $img, $x, $y, $width, $height
+        );
+    }
+}
+?>
