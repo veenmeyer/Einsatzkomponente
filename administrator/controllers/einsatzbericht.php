be175feddf7d1f60a375d8b93fb94e88633e59bb
Simon W <MrWeezle@users.noreply.github.com>
2015-07-16 14:37:03 +0200

    Update einsatzbericht.php

--- administrator/controllers/einsatzbericht.php
+++ administrator/controllers/einsatzbericht.php
@@ -113,9 +113,9 @@ class EinsatzkomponenteControllerEinsatzbericht extends JControllerForm
 	//Schriftart und -größe wird definiert 
 	$pdf->SetFont('Arial','',12);
 	//Header-Image
-	print($params->get('pdf_header'));
+	print("Image: ".$params->get('pdf_header')."--");
 	die();
-	$pdf->Image($params->get('pdf_header'),0,0)
+	$pdf->Image($params->get('pdf_header'),0,0);
 	
 	$pdf->Cell($breite,$höhe,'Einsatz-ID:');
 	$pdf->Cell($breite,$höhe,$id,0,1);
