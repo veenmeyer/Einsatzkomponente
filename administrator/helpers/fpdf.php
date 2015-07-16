82f070386efa69fbd1caf5809ffee93422af2101
Simon W <MrWeezle@users.noreply.github.com>
2015-07-16 14:59:35 +0200

    Update fpdf.php

--- administrator/helpers/fpdf.php
+++ administrator/helpers/fpdf.php
@@ -73,6 +73,34 @@ var $PDFVersion;         // PDF version number
 *                               Public methods                                 *
 *                                                                              *
 *******************************************************************************/
+
+const DPI = 96;
+const MM_IN_INCH = 25.4;
+const A4_HEIGHT = 297;
+const A4_WIDTH = 210;
+// tweak these values (in pixels)
+const MAX_WIDTH = 800;
+const MAX_HEIGHT = 500;
+function pixelsToMM($val) {
+	return $val * self::MM_IN_INCH / self::DPI;
+}
+function resizeToFit($imgFilename) {
+	list($width, $height) = getimagesize($imgFilename);
+	$widthScale = self::MAX_WIDTH / $width;
+	$heightScale = self::MAX_HEIGHT / $height;
+	$scale = min($widthScale, $heightScale);
+	return array(
+	    round($this->pixelsToMM($scale * $width)),
+	    round($this->pixelsToMM($scale * $height))
+	);
+}
+function resizeImage($img, $x, $y) {
+	list($width, $height) = $this->resizeToFit($img);
+	// you will probably want to swap the width/height
+	// around depending on the page's orientation
+	$this->Image($img, $x, $y, $width, $height);
+}
+
 function FPDF($orientation='P', $unit='mm', $size='A4')
 {
 	// Some checks
