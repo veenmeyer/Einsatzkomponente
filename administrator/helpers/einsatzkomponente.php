171903e60b5d2fe4bfb4ef6eaeaa0843e458eb39
Simon W <MrWeezle@users.noreply.github.com>
2015-08-31 12:34:14 +0200

    Bugfix
    Fehler korrigiert, der beim Export eines Berichts ohne Fahrzeuge oder Organisationen einen SQL-Fehler verursacht hat.

--- administrator/helpers/einsatzkomponente.php
+++ administrator/helpers/einsatzkomponente.php
@@ -1123,43 +1123,47 @@ endif;
 				}
 				//Ersetze doppelte Kommas durch ein einzelnes
 				$fahrzeuge = preg_replace('/,,+/', ',', $fahrzeuge);
-			
-			$query = "SELECT name FROM #__eiko_fahrzeuge WHERE id IN (".$fahrzeuge.")";
-			$db->setQuery($query);
-			$db->execute();
-			$anz_fahrz = $db->getNumRows();
-			$fahrz_arr = $db->loadObjectList();
-			$fahrz_all = "";
-			$i = 0;
-			foreach ($fahrz_arr as $key => $value) {
-			    if ($i == 0)
-			    	$fahrz_all .= "";
-			    else
-			    	$fahrz_all .= " ";
-			    $fahrz_all .= $value->name.",";
-			    $i += 1;
+				
+			if ($fahrzeuge != null && $fahrzeuge >= 0 && $fahrzeuge != "") {
+				$query = "SELECT name FROM #__eiko_fahrzeuge WHERE id IN (".$fahrzeuge.")";
+				$db->setQuery($query);
+				$db->execute();
+				$anz_fahrz = $db->getNumRows();
+				$fahrz_arr = $db->loadObjectList();
+				$fahrz_all = "";
+				$i = 0;
+				foreach ($fahrz_arr as $key => $value) {
+				    if ($i == 0)
+				    	$fahrz_all .= "";
+				    else
+				    	$fahrz_all .= " ";
+				    $fahrz_all .= $value->name.",";
+				    $i += 1;
+				}
+				//Entferne das Komma am Ende
+				$fahrz_all = substr($fahrz_all, 0, -1);
 			}
-			//Entferne das Komma am Ende
-			$fahrz_all = substr($fahrz_all, 0, -1);
 			
-			$query = "SELECT name FROM #__eiko_organisationen WHERE id IN (".$orgas.")";
-			$db->setQuery($query);
-			$db->execute();
-			$anz_orgas = $db->getNumRows();
-			$orga_arr = $db->loadObjectList();
-			$orgas_all = "";
-			$i = 0;
-			foreach ($orga_arr as $key => $value) {
-			    if ($i == 0)
-			    	$orgas_all .= "";
-			    else
-			    	$orgas_all .= " ";
-			    $orgas_all .= $value->name.",";
-			    $i += 1;
+			if ($fahrzeuge != null && $fahrzeuge >= 0 && $fahrzeuge != "") {
+				$query = "SELECT name FROM #__eiko_organisationen WHERE id IN (".$orgas.")";
+				$db->setQuery($query);
+				$db->execute();
+				$anz_orgas = $db->getNumRows();
+				$orga_arr = $db->loadObjectList();
+				$orgas_all = "";
+				$i = 0;
+				foreach ($orga_arr as $key => $value) {
+				    if ($i == 0)
+				    	$orgas_all .= "";
+				    else
+				    	$orgas_all .= " ";
+				    $orgas_all .= $value->name.",";
+				    $i += 1;
+				}
+				$i = 0;
+				//Entferne das Komma am Ende
+				$orgas_all = substr($orgas_all, 0, -1);
 			}
-			$i = 0;
-			//Entferne das Komma am Ende
-			$orgas_all = substr($orgas_all, 0, -1);
 			
 			//Variablendeklaraion für die PDF
 			$id = $einsatz[0]->id;
