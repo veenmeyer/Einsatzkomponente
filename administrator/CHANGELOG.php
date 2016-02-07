e038cd2b18c9d69bc15b312df5e9ab162bbd8bfb
Ralf Meyer <ralf.meyer@mail.de>
2016-02-07 11:38:14 +0100

    diverse kleinere Änderungen

---
+++ administrator/CHANGELOG.php
@@ -0,0 +1,7 @@
+<?php defined('_JEXEC') or die(); ?>
+<h1>Einsatzkomponente für das Joomla-CMS</h1>
+<h3>Changelog <small>Version 3.12</small></h3>
+<ul>
+<li> Changelog im Kontrollcenter hinzugefügt </li>
+<li> diverse kleinere Änderungen <li>
+</ul>
