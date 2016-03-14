ef5f4f0092d25804edc2415ce815d91d78771a7e
Ralf Meyer <ralf.meyer@mail.de>
2016-03-14 22:11:16 +0100

    Bugfix Detail_layout_4

--- administrator/CHANGELOG.php
+++ administrator/CHANGELOG.php
@@ -2,7 +2,8 @@
 <h1>Einsatzkomponente für das Joomla-CMS</h1>
 <h3>Changelog <small>Version 3.12</small></h3>
 <ul>
-<li> diverse kleinere Änderungen <li>
-<li> Entfernungsanzeige unter der Karte im Detail-Layout4 hinzugefügt <li>
-<li> diverse kleinere Änderungen im Detail-Layout4 <li>
+<li> diverse kleinere Änderungen </li>
+<li> Entfernungsanzeige unter der Karte im Detail-Layout4 hinzugefügt </li>
+<li> diverse kleinere Änderungen im Detail-Layout4 </li>
+<li> Jahreszahl-Image aus Detail-Layout4 entfernt</li>
 </ul>
