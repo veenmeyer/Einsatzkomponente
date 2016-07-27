c8f1e91311a0d5bcebf5a66b678ddf413349240b
Ralf Meyer <ralf.meyer@mail.de>
2016-07-27 12:39:44 +0200

    neue version bootstrap.min.css hinzugefügt

--- administrator/CHANGELOG.php
+++ administrator/CHANGELOG.php
@@ -2,11 +2,13 @@
 <h1>Einsatzkomponente für das Joomla-CMS</h1>
 <h3>Changelog <small>Version 3.13</small></h3>
 <ul>
++ fehlende index.html's im Frontend hinzugefüg
++ neue bootstrap.min.css V3.3.7 hinzugefügt
++ GoogleApiKey für Maps hinzugefügt
+# Links zu GoogleMaps geändert
 + Einsatznummern in der Einsatzübersicht
 # Backend-Übersichten Filter repariert 
 # Auswahl der Optionen bearbeitet
-# http gegen https - GoogleMaps ausgetauscht (Einsatzkarte)
-# http gegen https - GoogleMaps ausgetauscht (Einsatzedit-Front)
 + Entfernungsanzeige unter der Karte im Detail-Layout4 hinzugefügt
 # diverse kleinere Änderungen im Detail-Layout4 
 - Jahreszahl-Image aus Detail-Layout4 entfernt
