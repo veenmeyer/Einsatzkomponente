bdf558142dbefecc31ff5968d0de65c502cc1910
Ralf Meyer <ralf.meyer@mail.de>
2016-08-19 18:31:28 +0200

    Bugfix: Sortierung nach Counter-Anzahl

--- administrator/CHANGELOG.php
+++ administrator/CHANGELOG.php
@@ -2,6 +2,7 @@
 <h1>Einsatzkomponente für das Joomla-CMS</h1>
 <h3>Changelog <small>Version 3.13</small></h3>
 <ul>
+# Fehler in der Sortierung nach Counter-Anzahl behoben
 - neue bootstrap.min.css V3.3.7 (bringt zu viele Layoutfehler)
 # Fatal error: Call to a member function getData() in Fahrzeug-und Organisationsdetail
 # Senden-Button beim Frontend-Einsatzfahrzeug-Edit hinzugefügt
