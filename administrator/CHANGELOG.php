7814df989c78aa88b2d2f5a1749f8ee6a301ed54
Ralf Meyer <ralf.meyer@mail.de>
2017-06-28 11:39:44 +0200

    Bugfix: Menüpunkte: Detailansicht Organisation + Einsatzfahrzeug

--- administrator/CHANGELOG.php
+++ administrator/CHANGELOG.php
@@ -13,6 +13,7 @@
 
 <h3>Changelog <small>Version 3.17</small></h3>
 <ul>
++ Menüpunkte: Detailansicht Organisation + Einsatzfahrzeug
 + Optionen: Pre-Selektion Auswahlfeld Organisation im Einsatzbericht
 + Einsatzberichte: Erstellt von / Bearbeitet von
 + Option Einsatzbericht-Text anzeigen
